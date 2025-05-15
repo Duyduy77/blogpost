84a382f6f03b5e6b3ef5b7fbfec6d9ce5a228474
Duyduy <131287180+Duyduy77@users.noreply.github.com>
2025-05-15 07:44:25 +0700

    Create BlogPosts.js

---
+++ BlogPosts.js
@@ -0,0 +1,13 @@
+const BlogPosts = [
+  {
+    slug: "first-blog-post",
+    title: "First Blog Post",
+    description: "This is the first blog post",
+  },
+  {
+    slug: "second-blog-post",
+    title: "Second Blog Post",
+    description: " This is the second blog post",
+  },
+];
+module.exports = { BlogPosts };
